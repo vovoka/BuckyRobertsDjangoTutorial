c143b1e383be69fe91e4b1f9526997b2733da8e7
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 18:36:53 +0300

    Django Tutorial for Beginners - 13 - Connecting to the Database

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from music import views
 
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/id
+    # /music/id/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from .models import Album
 
 
 def index(request):
-    return HttpResponse('<h1>This is a music app homepage</h1>')
+
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="'+ url +'"> ' + album.album_title + ' </a><br>'
+    return HttpResponse(html)
+
 
 def detail(request, album_id):
     return HttpResponse('<h1>Details for album id ' + str(album_id) + ' </h1>')
