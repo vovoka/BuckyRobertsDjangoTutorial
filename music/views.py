166008be055c5d6fdd0bb36fe4a930e42ee26409
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 19:41:34 +0300

    Django Tutorial for Beginners - 15 - Render Template Shortcut

--- music/views.py
+++ music/views.py
@@ -1,17 +1,14 @@
 from django.http import HttpResponse
-from django.template import  loader
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
 
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums':all_albums,
-    }
+    context = {'all_albums':all_albums }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
