30158ea6045eba8066579751e861a10225083e57
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 19:51:26 +0300

    Django Tutorial for Beginners - 16 - Raising a 404 HTTP Error

--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -6,10 +6,12 @@ from .models import Album
 def index(request):
 
     all_albums = Album.objects.all()
-    context = {'all_albums':all_albums }
-
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums':all_albums })
 
 
 def detail(request, album_id):
-    return HttpResponse('<h1>Details for album id ' + str(album_id) + ' </h1>')
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exist')
+    return render(request, 'music/detail.html', {'album': album })
