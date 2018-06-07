c1938e98922080763b4ae3b068803d05ac36c47f
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 18:08:57 +0300

    Django Tutorial for Beginners - 12 - Writing Another View

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from music import views
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/id
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
