8c3c7348d1fb8e951059faf46e45fe528b34a5de
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 19:32:21 +0300

    Django Tutorial for Beginners - 14 - Templates

--- music/views.py
+++ music/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
+from django.template import  loader
 from .models import Album
 
 
 def index(request):
 
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="'+ url +'"> ' + album.album_title + ' </a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums':all_albums,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
