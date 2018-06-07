f2f5339f3923c51b172b46d729f20fbf8f7adb52
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-07 17:29:16 +0300

    Django Tutorial for Beginners - 7 - Creating Models (fixed Model mistype)

--- music/models.py
+++ music/models.py
@@ -9,6 +9,6 @@ class Album(models.Model):
 
 
 class Song(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE())
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
