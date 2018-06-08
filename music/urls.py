5be48d78813624dce699fd8d4a8d41f924c93567
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-08 18:47:26 +0300

    Django Tutorial for Beginners - 30 - Model Forms

--- music/urls.py
+++ music/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 ]
