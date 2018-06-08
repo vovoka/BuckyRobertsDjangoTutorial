902ef5897aff6092c773ab66723c6b493e8b241c
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2018-06-08 18:29:30 +0300

    Django Tutorial for Beginners - 29 - Generic Views

--- music/views.py
+++ music/views.py
@@ -1,30 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from .models import Album
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def index(request):
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albums':all_albums })
-
-
-def detail(request, album_id):
-    # album = Album.objects.get(pk=album_id)
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album })
-
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message':'You did nor select any song',
-        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-    return render(request, 'music/detail.html', {'album': album })
 
